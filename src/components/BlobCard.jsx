86d1a6f3db112c262d033ee43f65dad1bed54cb2
Daniel Maslowski <info@orangecms.org>
2020-09-20 01:37:54 +0200

    render binary names and EFI_FV type names

--- src/components/BlobCard.jsx
+++ src/components/BlobCard.jsx
@@ -1,24 +1,65 @@
-import React from "react";
+import React, { useState } from "react";
 import PropTypes from "prop-types";
 
-const BlobCard = ({ guid, type }) => {
+const hasName = (d) => d.Sections && d.Sections.find((s) => s.Name);
+
+const getName = (d) => {
+  const section = d.Sections.find((s) => s.Name);
+  return section.Name;
+};
+
+const DXEs = ({ dxes }) => {
+  const [open, setOpen] = useState(false);
+  const namedDxes = dxes.filter((d) => hasName(d));
+  return (
+    <>
+      <ul className={open ? "open" : ""} onClick={() => setOpen(!open)}>
+        {namedDxes.map((d) => (
+          <li key={d.Header.GUID.GUID}>{getName(d)}</li>
+        ))}
+      </ul>
+      <style jsx>{`
+        ul {
+          max-height: 135px;
+          overflow: hidden;
+        }
+        .open {
+          max-height: none;
+        }
+      `}</style>
+    </>
+  );
+};
+
+DXEs.propTypes = {
+  dxes: PropTypes.array,
+};
+
+const BlobCard = ({ guid, type, name, dxes }) => {
   return (
     <>
       <div className="card">
         <span className="type">{type}</span>
         <hr />
+        {name && <div className="name">{name}</div>}
         <span className="guid">{guid}</span>
+        {dxes && <DXEs dxes={dxes} />}
       </div>
       <style jsx>{`
         .card {
           border: 1px solid #422384;
           margin: 10px;
           padding: 4px;
+          min-width: 320px;
           width: 22%;
         }
         .type {
           background-color: #f1f2f3;
         }
+        .name {
+          color: #c02020;
+        }
+        .name,
         .guid {
           font-weight: bold;
         }
@@ -30,6 +71,8 @@ const BlobCard = ({ guid, type }) => {
 BlobCard.propTypes = {
   guid: PropTypes.string,
   type: PropTypes.number,
+  name: PropTypes.string,
+  dxes: PropTypes.array,
 };
 
 export default BlobCard;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -4,14 +4,40 @@ import PropTypes from "prop-types";
 import fixture from "../fixtures/A3MSTX_3.60.json";
 import BlobCard from "../components/BlobCard";
 
+const getDxes = (e) => {
+  const encaps = e.Sections.find((el) => el.Encapsulated);
+  const encapsulated = encaps && encaps.Encapsulated;
+
+  if (encapsulated && Array.isArray(encapsulated)) {
+    const encDxes = encapsulated.find(
+      (enc) => enc.Value.Type === "EFI_SECTION_FIRMWARE_VOLUME_IMAGE"
+    ).Value.Encapsulated[0].Value.Files;
+    return encDxes;
+  }
+  return null;
+};
+
 const FV = ({ guid, files }) => {
   return (
     <>
       <h3>{guid}</h3>
       <div className="FV">
         {files.map((e) => {
-          const { GUID, Type } = e.Header;
-          return <BlobCard key={GUID.GUID} guid={GUID.GUID} type={Type} />;
+          const { GUID } = e.Header;
+          const { Type } = e;
+          const section = e.Sections && e.Sections.find((s) => s.Name);
+          const name = section && section.Name;
+          const dxes =
+            Type === "EFI_FV_FILETYPE_FIRMWARE_VOLUME_IMAGE" ? getDxes(e) : 0;
+          return (
+            <BlobCard
+              key={GUID.GUID}
+              guid={GUID.GUID}
+              type={Type}
+              name={name}
+              dxes={dxes}
+            />
+          );
         })}
       </div>
       <style jsx>{`
