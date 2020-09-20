65644856e3f2fa3182b7aab3492da7360d7898d8
Daniel Maslowski <info@orangecms.org>
2020-09-20 19:33:02 +0200

    factor out DepEx component

--- src/components/DXEs.js
+++ src/components/DXEs.js
@@ -1,62 +1,15 @@
 import React, { useContext } from "react";
 import PropTypes from "prop-types";
+import classnames from "classnames";
+
 import {
-  getGuidFromDepEx,
   getGuidFromDxe,
   getName,
   getDepEx,
   hasName,
 } from "../util/dxe-helpers";
 import { GUIDContext } from "../context/GUIDContext";
-import classnames from "classnames";
-
-const DepEx = ({ depEx }) => {
-  // GUIDs may appear multuple times, hence can't be used as keys
-  const guidContext = useContext(GUIDContext);
-  const [contextGuid, setContextGuid] = guidContext;
-  return (
-    <>
-      <div className="depex">
-        <div className="headline">DepEx</div>
-        {depEx
-          .filter((d) => !!getGuidFromDepEx(d))
-          .map((d, i) => {
-            const guid = getGuidFromDepEx(d);
-            const className = classnames({ selected: contextGuid === guid });
-            return (
-              <div
-                className={className}
-                key={i}
-                onClick={() => setContextGuid(guid)}
-              >
-                &gt; {guid}
-              </div>
-            );
-          })}
-      </div>
-      <style jsx>{`
-        .depex {
-          background-color: #f0fcfd;
-          padding: 3px;
-          font-family: monospace;
-          font-size: 11px;
-        }
-        .headline {
-          text-align: center;
-          font-weight: bold;
-        }
-        .selected {
-          padding-left: 4px;
-          color: blue;
-        }
-      `}</style>
-    </>
-  );
-};
-
-DepEx.propTypes = {
-  depEx: PropTypes.array,
-};
+import DepEx from "./DepEx";
 
 const DXEs = ({ dxes, open = false }) => {
   const namedDxes = dxes.filter((d) => hasName(d));
