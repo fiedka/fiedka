18ba926a3cdcfab1e97d1fe75fbfc02b1345b5aa
Daniel Maslowski <info@orangecms.org>
2020-09-20 21:35:20 +0200

    factor out Volumes

---
+++ src/components/Volumes.jsx
@@ -0,0 +1,40 @@
+import React, { useContext } from "react";
+import PropTypes from "prop-types";
+import FV from "./FV";
+
+import { GUIDContext } from "../context/GUIDContext";
+
+const Volumes = ({ volumes }) => {
+  const guidContext = useContext(GUIDContext);
+  const [contextGuid, setContextGuid] = guidContext;
+  return (
+    <>
+      <header>
+        <button onClick={() => setContextGuid(null)}>Clear GUID</button>
+        ----{contextGuid}----
+      </header>
+      {volumes.map((fv) => {
+        const guid = fv.Value.FVName.GUID;
+        const files = fv.Value.Files || [];
+        return (
+          <span key={guid}>
+            <FV guid={guid} files={files} />
+          </span>
+        );
+      })}
+      <style jsx>{`
+        header {
+          background: #f0f0f0;
+          position: sticky;
+          top: 0;
+        }
+      `}</style>
+    </>
+  );
+};
+
+Volumes.propTypes = {
+  volumes: PropTypes.array,
+};
+
+export default Volumes;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,42 +1,8 @@
-import React, { useContext } from "react";
-import PropTypes from "prop-types";
+import React from "react";
 
 import fixture from "../fixtures/A3MSTX_3.60.json";
-import FV from "../components/FV";
-import { GUIDProvider, GUIDContext } from "../context/GUIDContext";
-
-const Volumes = ({ volumes }) => {
-  const guidContext = useContext(GUIDContext);
-  const [contextGuid, setContextGuid] = guidContext;
-  return (
-    <>
-      <header>
-        <button onClick={() => setContextGuid(null)}>Clear GUID</button>
-        ----{contextGuid}----
-      </header>
-      {volumes.map((fv) => {
-        const guid = fv.Value.FVName.GUID;
-        const files = fv.Value.Files;
-        return (
-          <span key={guid}>
-            <FV guid={guid} files={files} />
-          </span>
-        );
-      })}
-      <style jsx>{`
-        header {
-          background: #f0f0f0;
-          position: sticky;
-          top: 0;
-        }
-      `}</style>
-    </>
-  );
-};
-
-Volumes.propTypes = {
-  volumes: PropTypes.array,
-};
+import { GUIDProvider } from "../context/GUIDContext";
+import Volumes from "../components/Volumes";
 
 const Page = () => {
   const volumes = fixture.Elements.filter(
