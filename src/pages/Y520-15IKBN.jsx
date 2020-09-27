f0e00c9fb1f79574cda8511db4b371bfdeaf98b3
Daniel Maslowski <info@orangecms.org>
2020-09-27 19:39:38 +0200

    add fixture from Y520-15IKBN

---
+++ src/pages/Y520-15IKBN.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+
+import fixture from "../fixtures/Y520-15IKBN.json";
+import { GUIDProvider } from "../context/GUIDContext";
+import Volumes from "../components/Volumes";
+
+const Page = () => {
+  const volumes = fixture.Elements.filter(
+    (e) => e.Type === "*uefi.FirmwareVolume"
+  );
+
+  return (
+    <>
+      <div className="layout">
+        <div>
+          <GUIDProvider>
+            <Volumes volumes={volumes} />
+          </GUIDProvider>
+        </div>
+      </div>
+      <style jsx>{`
+        .layout {
+          display: flex;
+        }
+        aside {
+          float: left;
+        }
+      `}</style>
+    </>
+  );
+};
+
+export default Page;
