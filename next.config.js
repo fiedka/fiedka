0977eb859de3e7fc231b1739fb95e593e1d367c5
Daniel Maslowski <info@orangecms.org>
2020-09-27 19:40:40 +0200

    add base path to Next config
    see https://nextjs.org/docs/api-reference/next.config.js/basepath

--- next.config.js
+++ next.config.js
@@ -3,4 +3,5 @@ const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   // Use the CDN in production and localhost for development.
   assetPrefix: isProd ? "/utk-web" : "",
+  basePath: isProd ? "/utk-web" : "",
 };
