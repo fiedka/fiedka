f4e0d1080f57f9cc97e33788b1c13bb806a4f827
Daniel Maslowski <info@orangecms.org>
2020-10-08 19:48:45 +0200

    factor out PSP directory component

---
+++ src/PSP/PspDir.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+import PropTypes from "prop-types";
+import PspCard from "../components/PspCard";
+
+const hexify = (a) => a.toString(16);
+
+const PspDir = ({ dir }) => {
+  return (
+    <>
+      <div className="flex-around directory">
+        <h3>
+          <span>{`type: ${dir.directoryType}`}</span>
+          <span>{`magic: ${dir.magic}`}</span>
+          <span>{`address: 0x${hexify(dir.address)}`}</span>
+        </h3>
+        <div className="psps">
+          {dir.entries.map((p) => (
+            <PspCard psp={p} key={p.index} />
+          ))}
+        </div>
+      </div>
+      <style jsx>{`
+        .flex-around {
+          display: flex;
+          justify-content: space-around;
+        }
+        .directory {
+          border: 1px dashed #800020;
+          padding: 0 8px;
+          margin-bottom: 8px;
+        }
+        h3 {
+          display: flex;
+          flex-direction: column;
+          background-color: #dffcdf;
+        }
+        .psps {
+          display: flex;
+          flex-wrap: wrap;
+        }
+      `}</style>
+    </>
+  );
+};
+
+PspDir.propTypes = {
+  dir: PropTypes.object,
+};
+
+export default PspDir;
--- src/pages/A3MSTX_3.60.psp.jsx
+++ src/pages/A3MSTX_3.60.psp.jsx
@@ -2,54 +2,23 @@ import React from "react";
 
 import pspDirs from "../fixtures/A3MSTX_3.60.psp.json";
 import { PubKeyProvider } from "../context/PubKeyContext";
-import PspCard from "../components/PspCard";
-
-const hexify = (a) => a.toString(16);
+import PspDir from "../PSP/PspDir";
 
 const Page = () => {
   return (
     <>
       <div className="layout">
-        <div>
-          <PubKeyProvider>
+        <PubKeyProvider>
+          <div>
             {pspDirs.map((d) => (
-              <div key={d.directory} className="flex-around directory">
-                <h3>
-                  <span>{`type: ${d.directoryType}`}</span>
-                  <span>{`magic: ${d.magic}`}</span>
-                  <span>{`address: 0x${hexify(d.address)}`}</span>
-                </h3>
-                <div className="psps">
-                  {d.entries.map((p) => (
-                    <PspCard psp={p} key={p.index} />
-                  ))}
-                </div>
-              </div>
+              <PspDir key={d.directory} dir={d} />
             ))}
-          </PubKeyProvider>
-        </div>
+          </div>
+        </PubKeyProvider>
       </div>
       <style jsx>{`
         .layout {
           display: flex;
-        }
-        .flex-around {
-          display: flex;
-          justify-content: space-around;
-        }
-        .directory {
-          border: 1px dashed #800020;
-          padding: 0 8px;
-          margin-bottom: 8px;
-        }
-        h3 {
-          display: flex;
-          flex-direction: column;
-          background-color: #dffcdf;
-        }
-        .psps {
-          display: flex;
-          flex-wrap: wrap;
         }
       `}</style>
     </>
