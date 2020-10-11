f198513c94eb29bb5c606afb738e171ba4367c61
Daniel Maslowski <info@orangecms.org>
2020-10-11 18:27:44 +0200

    FlashUsage: add size in M and percentage

--- src/components/FlashUsage.jsx
+++ src/components/FlashUsage.jsx
@@ -7,30 +7,42 @@ const FlashUsage = ({ usage }) => {
     const cols = entries.map((e, j) => <td key={j} className={e}></td>);
     return <tr key={i}>{cols}</tr>;
   });
+
+  const percentage = (x) => Math.round((x / blocks) * 10000) / 100;
+  const size = (x) => Math.round((x / 256) * 100) / 100;
+
   return (
     <>
       <table className="legend">
         <tr>
           <th>blocks</th>
           <td>{blocks}</td>
+          <td>100%</td>
+          <td>{size(blocks)}M</td>
         </tr>
         <tr>
           <th>
             <div className="zero square"></div> zero (<pre>0x00</pre>)
           </th>
           <td>{zero}</td>
+          <td>{percentage(zero)}%</td>
+          <td>{size(zero)}M</td>
         </tr>
         <tr>
           <th>
             <div className="full square"></div> full (<pre>0xff</pre>)
           </th>
           <td>{full}</td>
+          <td>{percentage(full)}%</td>
+          <td>{size(full)}M</td>
         </tr>
         <tr>
           <th>
             <div className="used square"></div> used
           </th>
           <td>{used}</td>
+          <td>{percentage(used)}%</td>
+          <td>{size(used)}M</td>
         </tr>
       </table>
       <table>
