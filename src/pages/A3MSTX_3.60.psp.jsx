32e4be5a5743fdcf8251c4910b3e4c856be62e62
Daniel Maslowski <info@orangecms.org>
2020-10-18 16:22:24 +0200

    A3MSTX_3.60 PSP: add flash usage

--- src/pages/A3MSTX_3.60.psp.jsx
+++ src/pages/A3MSTX_3.60.psp.jsx
@@ -1,24 +1,36 @@
 import React from "react";
 
 import pspDirs from "../fixtures/A3MSTX_3.60.psp.json";
+import usage from "../fixtures/A3MSTX_3.60.fmap_usage.json";
 import { PubKeyProvider } from "../context/PubKeyContext";
+import { MarkedEntriesProvider } from "../context/MarkedEntriesContext";
+import FlashUsage from "../components/FlashUsage";
 import PspDir from "../PSP/PspDir";
 
 const Page = () => {
   return (
     <>
-      <div className="layout">
-        <PubKeyProvider>
-          <div>
-            {pspDirs.map((d) => (
-              <PspDir key={d.directory} dir={d} />
-            ))}
-          </div>
-        </PubKeyProvider>
-      </div>
+      <MarkedEntriesProvider>
+        <div className="layout">
+          <PubKeyProvider>
+            <div>
+              {pspDirs.map((d) => (
+                <PspDir key={d.directory} dir={d} />
+              ))}
+            </div>
+          </PubKeyProvider>
+          <aside>
+            <h2>Flash Usage</h2>
+            <FlashUsage usage={usage} />
+          </aside>
+        </div>
+      </MarkedEntriesProvider>
       <style jsx>{`
         .layout {
           display: flex;
+        }
+        aside {
+          float: left;
         }
       `}</style>
     </>
