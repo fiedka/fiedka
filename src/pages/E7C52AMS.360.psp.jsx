d6fe914baa742ff3ab7dd5150c9f9139c99c422b
Daniel Maslowski <info@orangecms.org>
2020-10-18 16:53:07 +0200

    add flash usage to other PSP pages

--- src/pages/E7C52AMS.360.psp.jsx
+++ src/pages/E7C52AMS.360.psp.jsx
@@ -1,24 +1,36 @@
 import React from "react";
 
 import pspDirs from "../fixtures/E7C52AMS.360.psp.json";
+import usage from "../fixtures/E7C52AMS.360.fmap_usage.json";
 import { PubKeyProvider } from "../context/PubKeyContext";
+import { MarkedEntriesProvider } from "../context/MarkedEntriesContext";
+import FlashUsage from "../components/FlashUsage";
 import PspDir from "../PSP/PspDir";
 
 const Page = () => {
   return (
     <>
-      <div className="layout">
-        <PubKeyProvider>
-          <div>
-            {pspDirs.map((d) => (
-              <PspDir key={d.directory} dir={d} />
-            ))}
-          </div>
-        </PubKeyProvider>
-      </div>
+      <MarkedEntriesProvider>
+        <div className="layout">
+          <PubKeyProvider>
+            <div>
+              {pspDirs.map((d) => (
+                <PspDir key={d.directory} dir={d} />
+              ))}
+            </div>
+          </PubKeyProvider>
+          <aside>
+            <h2>Flash Usage</h2>
+            <FlashUsage usage={usage} />
+          </aside>
+        </div>
+      </MarkedEntriesProvider>
       <style jsx>{`
         .layout {
           display: flex;
+        }
+        aside {
+          float: left;
         }
       `}</style>
     </>
--- src/pages/MZ92-FS0_R16_F01.psp.jsx
+++ src/pages/MZ92-FS0_R16_F01.psp.jsx
@@ -1,24 +1,36 @@
 import React from "react";
 
 import pspDirs from "../fixtures/MZ92-FS0_R16_F01.psp.json";
+import usage from "../fixtures/MZ92-FS0_R16_F01.fmap_usage.json";
 import { PubKeyProvider } from "../context/PubKeyContext";
+import { MarkedEntriesProvider } from "../context/MarkedEntriesContext";
+import FlashUsage from "../components/FlashUsage";
 import PspDir from "../PSP/PspDir";
 
 const Page = () => {
   return (
     <>
-      <div className="layout">
-        <PubKeyProvider>
-          <div>
-            {pspDirs.map((d) => (
-              <PspDir key={d.directory} dir={d} />
-            ))}
-          </div>
-        </PubKeyProvider>
-      </div>
+      <MarkedEntriesProvider>
+        <div className="layout">
+          <PubKeyProvider>
+            <div>
+              {pspDirs.map((d) => (
+                <PspDir key={d.directory} dir={d} />
+              ))}
+            </div>
+          </PubKeyProvider>
+          <aside>
+            <h2>Flash Usage</h2>
+            <FlashUsage usage={usage} />
+          </aside>
+        </div>
+      </MarkedEntriesProvider>
       <style jsx>{`
         .layout {
           display: flex;
+        }
+        aside {
+          float: left;
         }
       `}</style>
     </>
