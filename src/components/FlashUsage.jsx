36b55fff7a62dd467f59d4bac1657c712fc1ed9d
Daniel Maslowski <info@orangecms.org>
2020-10-18 16:25:08 +0200

    FlashUsage: add address and highlight marked/hovered blocks

--- src/components/FlashUsage.jsx
+++ src/components/FlashUsage.jsx
@@ -1,12 +1,43 @@
-import React from "react";
+import React, { useContext } from "react";
 import PropTypes from "prop-types";
+import cn from "classnames";
+
 import colors from "../util/colors";
+import { MarkedEntriesContext } from "../context/MarkedEntriesContext";
+
+// TODO
+const isMarked = (markedBlocks, block, offset) =>
+  markedBlocks.includes(block + offset);
+
+// https://dev.to/ycmjason/how-to-create-range-in-javascript-539i
+const getMarkedBlocks = ({ address, length }) => {
+  const firstBlock = Math.floor(address / 4096);
+  const lastBlock = Math.floor((address + length) / 4096);
+  const blockCount = lastBlock - firstBlock;
+  return Array.from({ length: blockCount + 1 }, (_, i) => firstBlock + i);
+};
 
 const FlashUsage = ({ usage }) => {
+  const markedEntriesContext = useContext(MarkedEntriesContext);
+  const { hoveredEntry, markedEntries } = markedEntriesContext;
+  const hoveredBlocks = (hoveredEntry && getMarkedBlocks(hoveredEntry)) || [];
+  const markedBlocks =
+    (markedEntries && markedEntries.map((e) => getMarkedBlocks(e)).flat()) ||
+    [];
   const { layout, blocks, zero, full, used } = usage;
-  const rows = layout.map(({ entries }, i) => {
-    const cols = entries.map((e, j) => <td key={j} className={e}></td>);
-    return <tr key={i}>{cols}</tr>;
+  const rows = layout.map(({ address, entries }, i) => {
+    const cols = entries.map((e, j) => {
+      const block = parseInt(address) / 4096; // FIXME: not necessary *here*
+      const hovered = hoveredEntry && isMarked(hoveredBlocks, block, j);
+      const marked = isMarked(markedBlocks, block, j);
+      return <td key={j} className={cn(e, { hovered, marked })}></td>;
+    });
+    return (
+      <tr key={i}>
+        <td className="address">{address}</td>
+        {cols}
+      </tr>
+    );
   });
 
   const percentage = (x) => Math.round((x / blocks) * 10000) / 100;
@@ -52,7 +83,8 @@ const FlashUsage = ({ usage }) => {
       <style jsx>
         {`
           table {
-            width: 320px;
+            width: 420px;
+            margin: 5px 0;
             padding: 0;
             font-family: sans-serif;
           }
@@ -75,7 +107,10 @@ const FlashUsage = ({ usage }) => {
       <style jsx global>
         {`
           td {
+            width: 10px;
             height: 8px;
+            padding: 0;
+            overflow: hidden;
           }
           .used {
             background-color: ${colors[25]};
@@ -85,6 +120,19 @@ const FlashUsage = ({ usage }) => {
           }
           .zero {
             background-color: ${colors[9]};
+          }
+          .marked {
+            background-color: ${colors[4]};
+          }
+          .hovered {
+            background-color: ${colors[2]};
+          }
+          .address {
+            font-family: monospace;
+            font-size: 10px;
+            font-weight: bold;
+            width: 60px;
+            height: 8px;
           }
         `}
       </style>
