d91dc81f1e8572b31adf9eacff18acdde344769d
Daniel Maslowski <info@orangecms.org>
2020-11-02 00:28:48 +0100

    redo templates for new UI

--- genpages.sh
+++ genpages.sh
@@ -2,12 +2,36 @@
 
 if [ $# -eq 0 ]
   then
-    echo "Please provide the name of you firmware image"
+    echo "Please provide the name of your firmware image."
+    exit 1
 fi
 
 FIRMWARE_IMAGE=$1
 
-sed "s#{FIRMWARE_IMAGE}#${FIRMWARE_IMAGE}#" \
-  src/templates/UEFI.tmpl > src/pages/"${FIRMWARE_IMAGE}.jsx"
-sed "s#{FIRMWARE_IMAGE}#${FIRMWARE_IMAGE}#" \
-  src/templates/PSP.tmpl > src/pages/"${FIRMWARE_IMAGE}.psp.jsx"
+case $2 in
+  "all")
+    echo "Generating ${FIRMWARE_IMAGE}.all"
+    sed "s#{FIRMWARE_IMAGE}#${FIRMWARE_IMAGE}#" \
+      src/templates/all.tmpl > src/pages/"${FIRMWARE_IMAGE}.all.jsx"
+    ;;
+  "psp")
+    echo "Generating ${FIRMWARE_IMAGE}.psp"
+    sed "s#{FIRMWARE_IMAGE}#${FIRMWARE_IMAGE}#" \
+      src/templates/psp.tmpl > src/pages/"${FIRMWARE_IMAGE}.psp.jsx"
+    ;;
+  "uefi")
+    echo "Generating ${FIRMWARE_IMAGE}.uefi"
+    sed "s#{FIRMWARE_IMAGE}#${FIRMWARE_IMAGE}#" \
+      src/templates/uefi.tmpl > src/pages/"${FIRMWARE_IMAGE}.uefi.jsx"
+    ;;
+  "ufp")
+    echo "Generating ${FIRMWARE_IMAGE}.ufp"
+    sed "s#{FIRMWARE_IMAGE}#${FIRMWARE_IMAGE}#" \
+      src/templates/ufp.tmpl > src/pages/"${FIRMWARE_IMAGE}.ufp.jsx"
+    ;;
+  *)
+    echo "Generating ${FIRMWARE_IMAGE}"
+    sed "s#{FIRMWARE_IMAGE}#${FIRMWARE_IMAGE}#" \
+      src/templates/utk.tmpl > src/pages/"${FIRMWARE_IMAGE}.jsx"
+    ;;
+esac
