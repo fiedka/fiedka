bbef9118df810ad2291ce8a9479102ceeb838ec5
Daniel Maslowski <info@orangecms.org>
2020-11-08 04:00:31 +0100

    redo OVMF ufp fixture and page

--- src/pages/ovmf-202005.ufp.jsx
+++ src/pages/ovmf-202005.ufp.jsx
@@ -1,32 +1,39 @@
 import React from "react";
+import { Tabs } from "@coalmines/indui";
 
-import fixture from "../fixtures/ovmf-202005.ufp.json";
+import ufpFixture from "../fixtures/ovmf-202005.ufp.json";
+import utkFixture from "../fixtures/ovmf-202005.utk.json";
 import usage from "../fixtures/ovmf-202005.fmap.json";
+import { transform as ufpTransform } from "../util/ufp";
+import { transform as utkTransform } from "../util/utk";
 import { MarkedEntriesProvider } from "../context/MarkedEntriesContext";
 import { GUIDProvider } from "../context/GUIDContext";
-import FirmwareVolume from "../UEFI/FV";
-import FlashUsage from "../components/FlashUsage";
 import Layout from "../components/Layout";
-import { transform } from "../util/ufp";
+import FlashUsage from "../components/FlashUsage";
+import FirmwareVolumes from "../UEFI/FirmwareVolumes";
+const utkVolumes = utkTransform(
+  utkFixture.Elements.filter((e) => e.Type === "*uefi.FirmwareVolume")
+);
 
-const fvs = transform(fixture);
+const uefi = ufpFixture.regions.find((r) => r.type === "bios");
+const ufpVolumes = ufpTransform(uefi.data.firmwareVolumes);
 
 const Page = () => {
-  const volumes = fvs.map(({ guid, parentGuid, size, checksum, ffs }) => (
-    <FirmwareVolume
-      key={checksum}
-      guid={guid}
-      parentGuid={parentGuid}
-      size={size}
-      ffs={ffs}
-    />
-  ));
+  const ufp = <FirmwareVolumes fvs={ufpVolumes} />;
+  const utk = <FirmwareVolumes fvs={utkVolumes} />;
 
   return (
     <MarkedEntriesProvider>
       <Layout sidepane={<FlashUsage usage={usage} />}>
         <GUIDProvider>
-          <div>{volumes}</div>
+          <div>
+            <Tabs
+              menu={[
+                { id: "ufp", body: ufp, label: "uefi-firmware-parser" },
+                { id: "utk", body: utk, label: "utk" },
+              ]}
+            />
+          </div>
         </GUIDProvider>
       </Layout>
     </MarkedEntriesProvider>
