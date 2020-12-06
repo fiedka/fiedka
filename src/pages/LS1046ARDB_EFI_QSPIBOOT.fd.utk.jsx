2b0144d3eb13b7894283f641f1f97565a961910a
Daniel Maslowski <info@orangecms.org>
2020-12-06 21:14:31 +0100

    migrate away from and remove old Volumes component

--- src/pages/LS1046ARDB_EFI_QSPIBOOT.fd.utk.jsx
+++ src/pages/LS1046ARDB_EFI_QSPIBOOT.fd.utk.jsx
@@ -1,24 +1,25 @@
 import React from "react";
 
-import fixture from "../fixtures/LS1046ARDB_EFI_QSPIBOOT.fd.utk.json";
+import utkFixture from "../fixtures/LS1046ARDB_EFI_QSPIBOOT.fd.utk.json";
 import usage from "../fixtures/LS1046ARDB_EFI_QSPIBOOT.fd.fmap.json";
+import { transform as utkTransform } from "../util/utk";
 import { MarkedEntriesProvider } from "../context/MarkedEntriesContext";
 import { GUIDProvider } from "../context/GUIDContext";
-import Volumes from "../components/Volumes";
-import FlashUsage from "../components/FlashUsage";
 import Layout from "../components/Layout";
-import { transform } from "../util/utk";
+import FlashUsage from "../components/FlashUsage";
+import FirmwareVolumes from "../UEFI/FirmwareVolumes";
 
-const volumes = transform(
-  fixture.Elements.filter((e) => e.Type === "*uefi.FirmwareVolume")
+const utkVolumes = utkTransform(
+  utkFixture.Elements.filter((e) => e.Type === "*uefi.FirmwareVolume")
 );
 
 const Page = () => {
+  const utk = <FirmwareVolumes fvs={utkVolumes} />;
   return (
     <MarkedEntriesProvider>
       <Layout sidepane={<FlashUsage usage={usage} />}>
         <GUIDProvider>
-          <Volumes volumes={volumes} />
+          <div>{utk}</div>
         </GUIDProvider>
       </Layout>
     </MarkedEntriesProvider>
--- src/pages/P34V2BF.D08.utk.jsx
+++ src/pages/P34V2BF.D08.utk.jsx
@@ -1,25 +1,30 @@
 import React from "react";
 
-import fixture from "../fixtures/P34V2BF.D08.utk.json";
+import utkFixture from "../fixtures/P34V2BF.D08.utk.json";
 import usage from "../fixtures/P34V2BF.D08.fmap.json";
+import { transform as utkTransform } from "../util/utk";
 import { MarkedEntriesProvider } from "../context/MarkedEntriesContext";
 import { GUIDProvider } from "../context/GUIDContext";
-import Volumes from "../components/Volumes";
-import FlashUsage from "../components/FlashUsage";
 import Layout from "../components/Layout";
-import { transform } from "../util/utk";
+import FlashUsage from "../components/FlashUsage";
+import FirmwareVolumes from "../UEFI/FirmwareVolumes";
 
-const uefiRegion = fixture.Regions.find((r) => r.Type === "*uefi.BIOSRegion");
-const volumes = transform(
+// This is different from others becomes it comes from a full image with all
+// sections. TODO: move out helpers for general use
+const uefiRegion = utkFixture.Regions.find(
+  (r) => r.Type === "*uefi.BIOSRegion"
+);
+const utkVolumes = utkTransform(
   uefiRegion.Value.Elements.filter((e) => e.Type === "*uefi.FirmwareVolume")
 );
 
 const Page = () => {
+  const utk = <FirmwareVolumes fvs={utkVolumes} />;
   return (
     <MarkedEntriesProvider>
       <Layout sidepane={<FlashUsage usage={usage} />}>
         <GUIDProvider>
-          <Volumes volumes={volumes} />
+          <div>{utk}</div>
         </GUIDProvider>
       </Layout>
     </MarkedEntriesProvider>
