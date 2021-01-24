5632697b598f5cb20d40522a7d31bb4d855cb808
Daniel Maslowski <info@orangecms.org>
2021-01-24 22:42:44 +0100

    PspCard: extend with keys

--- src/PSP/PspCard.jsx
+++ src/PSP/PspCard.jsx
@@ -10,16 +10,38 @@ import colors from "../util/colors";
 
 const getSigKey = (info) => {
   const sig = info.find((i) => i.includes("signed"));
-  // FIXME: just a quick hack; work on PSPTool to output the key directly
+  // FIXME: just a quick hack; work on PSPTool to output the full key directly
   if (sig) {
     return sig.substr(7, 4);
   }
 };
 
+const shorten = (s) => s && s.substr(0, 8).toUpperCase();
+
+const getMeta = (meta) => {
+  if (!meta) {
+    return {};
+  }
+  const signature = shorten(meta.signature);
+  const sigFingerprint = shorten(meta.sigFingerprint);
+  const encFingerprint = shorten(meta.encFingerprint);
+  return { signature, sigFingerprint, encFingerprint };
+};
+
 const PspCard = ({ psp, open = true }) => {
   const pubKeyContext = useContext(PubKeyContext);
   const [contextPubKey, setContextPubKey] = pubKeyContext;
-  const { address, size, sectionType, magic, version, info, md5, sizes } = psp;
+  const {
+    address,
+    size,
+    sectionType,
+    magic,
+    version,
+    info,
+    md5,
+    sizes,
+    meta,
+  } = psp;
 
   const entry = { address, length: size };
 
@@ -34,7 +56,8 @@ const PspCard = ({ psp, open = true }) => {
   };
   const typeEmoji = isKey ? <span onClick={setPubKey}>🔑</span> : null;
 
-  const sigKey = getSigKey(info);
+  const { signature, sigFingerprint, encFingerprint } = getMeta(meta);
+  const sigKey = getSigKey(info) || sigFingerprint; // TODO
   const signed = typeof sigKey === "string";
   const selected = contextPubKey && sigKey === contextPubKey;
   const verified = info.find((i) => i.includes("verified"));
@@ -127,12 +150,12 @@ const PspCard = ({ psp, open = true }) => {
                 <td>0x{address.toString(16)}</td>
               </tr>
               <tr>
-                <th>size</th>
-                <td>{size}</td>
-              </tr>
-              <tr>
                 <th>hash</th>
                 <td>{md5}</td>
+              </tr>
+              <tr>
+                <th>size</th>
+                <td>{size}</td>
               </tr>
               {sizes && (
                 <>
@@ -155,7 +178,11 @@ const PspCard = ({ psp, open = true }) => {
           <span className="extra">
             {version && <div className="info">version {version}</div>}
             {magic && <div className="info">magic: {magic}</div>}
-            {sigKey && <div className="info">signature: {sigKey}</div>}
+            {signature && <div className="info">signature: {signature}</div>}
+            {sigKey && <div className="info">signing key: {sigKey}</div>}
+            {encFingerprint && (
+              <div className="info">encryption key: {encFingerprint}</div>
+            )}
             <div className="info">
               <Blocks size={size} />
             </div>
