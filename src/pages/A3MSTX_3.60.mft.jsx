70fff9e1909ec14de95108fac93d0a0f91ad2e90
Daniel Maslowski <info@orangecms.org>
2021-01-28 17:10:49 +0100

    add MFT page for A3MSTX_3.60

---
+++ src/pages/A3MSTX_3.60.mft.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+import mftFixture from "../fixtures/A3MSTX_3.60.mft.json";
+import usage from "../fixtures/A3MSTX_3.60.fmap.json";
+import { MarkedEntriesProvider } from "../context/MarkedEntriesContext";
+import FlashUsage from "../components/FlashUsage";
+import Layout from "../components/Layout";
+import PSPImage from "../PSP/PspImage";
+import { transform as mftTransform } from "../util/mft";
+
+const pspDirs = mftTransform(mftFixture);
+
+const Page = () => {
+  return (
+    <MarkedEntriesProvider>
+      <Layout sidepane={<FlashUsage usage={usage} />}>
+        <PSPImage directories={pspDirs} />
+      </Layout>
+    </MarkedEntriesProvider>
+  );
+};
+
+export default Page;
