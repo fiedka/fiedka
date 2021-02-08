c5ebea298c989476057cba68a121271795356884
Daniel Maslowski <info@orangecms.org>
2021-02-08 02:28:03 +0100

    add A3MSTX_3.70 MFT fixture

--- src/pages/A3MSTX_3.70.all.jsx
+++ src/pages/A3MSTX_3.70.all.jsx
@@ -4,9 +4,11 @@ import { Tabs } from "@coalmines/indui";
 import ufpFixture from "../fixtures/A3MSTX_3.70.ufp.json";
 import utkFixture from "../fixtures/A3MSTX_3.70.utk.json";
 import pspFixture from "../fixtures/A3MSTX_3.70.psp.json";
+import mftFixture from "../fixtures/A3MSTX_3.70.mft.json";
 import usage from "../fixtures/A3MSTX_3.70.fmap.json";
 import { transform as ufpTransform } from "../util/ufp";
 import { transform as utkTransform } from "../util/utk";
+import { transform as mftTransform } from "../util/mft";
 import { MarkedEntriesProvider } from "../context/MarkedEntriesContext";
 import { GUIDProvider } from "../context/GUIDContext";
 import Layout from "../components/Layout";
@@ -21,10 +23,13 @@ const utkVolumes = utkTransform(
 const uefi = ufpFixture.regions.find((r) => r.type === "bios");
 const ufpVolumes = ufpTransform(uefi.data.firmwareVolumes);
 
+const pspDirs = mftTransform(mftFixture);
+
 const Page = () => {
   const ufp = <FirmwareVolumes fvs={ufpVolumes} />;
   const utk = <FirmwareVolumes fvs={utkVolumes} />;
   const psp = <PSPImage directories={pspFixture} />;
+  const mft = <PSPImage directories={pspDirs} />;
 
   return (
     <MarkedEntriesProvider>
@@ -36,6 +41,7 @@ const Page = () => {
                 { id: "ufp", body: ufp, label: "uefi-firmware-parser" },
                 { id: "utk", body: utk, label: "utk" },
                 { id: "psp", body: psp, label: "psp" },
+                { id: "mft", body: mft, label: "MFT" },
               ]}
             />
           </div>
