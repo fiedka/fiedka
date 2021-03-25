3334025e519098fae454962c7db19a4b3cd1c5cb
Daniel Maslowski <info@orangecms.org>
2021-03-25 22:17:40 +0100

    add MSI X570-A Pro fixtures and pages

---
+++ src/pages/E7C37AMS.HD1.psp.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+import pspDirs from "../fixtures/E7C37AMS.HD1.psp.json";
+import usage from "../fixtures/E7C37AMS.HD1.fmap.json";
+import { MarkedEntriesProvider } from "../context/MarkedEntriesContext";
+import FlashUsage from "../components/FlashUsage";
+import Layout from "../components/Layout";
+import PSPImage from "../PSP/PspImage";
+
+const Page = () => {
+  return (
+    <MarkedEntriesProvider>
+      <Layout sidepane={<FlashUsage usage={usage} />}>
+        <PSPImage directories={pspDirs} />
+      </Layout>
+    </MarkedEntriesProvider>
+  );
+};
+
+export default Page;
