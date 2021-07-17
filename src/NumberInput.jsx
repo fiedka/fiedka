6cb5e09a92aefc32216d951121ad57da0d383a54
Daniel Maslowski <info@orangecms.org>
2021-07-17 23:23:10 +0200

    remove component from original example

--- src/NumberInput.jsx
+++
@@ -1,7 +0,0 @@
-import * as React from 'react';
-
-const NumberInput = ({ value, onChange }) => (
-    <input type="number" value={value} onChange={(e) => onChange(parseInt(e.target.value, 10))} />
-);
-
-export default NumberInput;
