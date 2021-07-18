dbafda70ea48aeef8deebb063f094270a13919e8
Daniel Maslowski <info@orangecms.org>
2021-07-18 13:54:49 +0200

    UEFIImage: handle nested BIOS region in Intel images

--- src/UEFIImage.jsx
+++ src/UEFIImage.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 import { transform as utkTransform } from "./util/utk";
 import { MarkedEntriesProvider } from "./context/MarkedEntriesContext";
 import { GUIDProvider } from "./context/GUIDContext";
@@ -7,10 +7,30 @@ import Layout from "./components/Layout";
 import FlashUsage from "./components/FlashUsage";
 import FirmwareVolumes from "./UEFI/FirmwareVolumes";
 
+const getUEFIElements = (data) => {
+  if (data.Regions) {
+    const region = data.Regions.find((r) => r.Type === "*uefi.BIOSRegion");
+    if (region && region.Value) {
+      return region.Value.Elements;
+    }
+  }
+  if (data.Elements) {
+    return data.Elements;
+  }
+  return null;
+};
+
+const getFVs = (data) => {
+  const elements = getUEFIElements(data);
+  if (elements) {
+    const fvs = elements.filter((e) => e.Type === "*uefi.FirmwareVolume");
+    return utkTransform(fvs);
+  }
+  return [];
+};
+
 const UEFIImage = ({ data, fmap, name }) => {
-  const fvs = utkTransform(
-    data.Elements.filter((e) => e.Type === "*uefi.FirmwareVolume")
-  );
+  const fvs = getFVs(data);
 
   return (
     <MarkedEntriesProvider>
@@ -21,6 +41,12 @@ const UEFIImage = ({ data, fmap, name }) => {
       </Layout>
     </MarkedEntriesProvider>
   );
+};
+
+UEFIImage.propTypes = {
+  data: PropTypes.object,
+  fmap: PropTypes.object,
+  name: PropTypes.string,
 };
 
 export default UEFIImage;
