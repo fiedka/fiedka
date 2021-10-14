14791b0a9fe58b728bdd247c818e90350f71d768
Daniel Maslowski <info@orangecms.org>
2021-10-14 23:02:29 +0200

    PSP/PspDir: extend stories with CSS fixtures

--- src/PSP/PspDir.stories.jsx
+++ src/PSP/PspDir.stories.jsx
@@ -4,6 +4,9 @@ import mftEntry from "../util/mft-entry.fixture.json";
 import mftEntryNoHeader from "../util/mft-entry-no-header.fixture.json";
 import mftEntryNoName from "../util/mft-entry-no-name.fixture.json";
 import { transformDir } from "../util/mft";
+import t14css from "../util/T14G1AMD_css.json";
+import a300css from "../util/A300_css.json";
+import { transformAmdFw } from "../util/css";
 import { MarkedEntriesProvider } from "../context/MarkedEntriesContext.js";
 import PspDir from "./PspDir";
 
@@ -29,5 +32,33 @@ const dirWithEntries = transformDir({
 export const withEntries = () => (
   <MarkedEntriesProvider>
     <PspDir dir={dirWithEntries} />
+  </MarkedEntriesProvider>
+);
+
+const cssDirs = transformAmdFw(a300css);
+
+export const css1 = () => (
+  <MarkedEntriesProvider>
+    <PspDir dir={cssDirs[0]} />
+  </MarkedEntriesProvider>
+);
+
+export const css2 = () => (
+  <MarkedEntriesProvider>
+    <PspDir dir={cssDirs[1]} />
+  </MarkedEntriesProvider>
+);
+
+export const css3 = () => (
+  <MarkedEntriesProvider>
+    <PspDir dir={cssDirs[2]} />
+  </MarkedEntriesProvider>
+);
+
+const cssDirsT14 = transformAmdFw(t14css);
+
+export const css4 = () => (
+  <MarkedEntriesProvider>
+    <PspDir dir={cssDirsT14[3]} />
   </MarkedEntriesProvider>
 );
