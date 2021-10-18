ca41feeb6bb873f77c5cc98b1212a5512e80959b
Daniel Maslowski <info@orangecms.org>
2021-10-18 23:46:22 +0200

    App: integrate amdana and AMDImage

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,53 @@
 import React, { useEffect, useState } from "react";
-import { Button, LoadingAnimation } from "@coalmines/indui";
+import PropTypes from "prop-types";
+import { Button, LoadingAnimation, Tabs, TextLine } from "@coalmines/indui";
 import { useFilePicker } from "use-file-picker";
 import wasm from "./main.go";
 import UEFIImage from "./UEFIImage";
+import AMDImage from "./AMDImage";
 import colors from "./util/colors";
 
-const { fmap, utka } = wasm;
+const { fmap, utka, amdana } = wasm;
+
+// use this instead of the real utka for testing only amdana
+// const utka = () => Promise.reject("Skipping UEFI analysis");
+
+const Main = ({ data, fileName }) => {
+  const { amd, fmap, intel, uefi } = data;
+  const menu = [];
+  if (uefi) {
+    menu.push({
+      id: "uefi",
+      body: <UEFIImage data={uefi} fmap={fmap} name={fileName} />,
+      label: "UEFI",
+    });
+  }
+  if (amd) {
+    menu.push({
+      id: "amd",
+      body: <AMDImage data={amd} fmap={fmap} name={fileName} />,
+      label: "AMD",
+    });
+  }
+  // TODO: Add Intel ME support
+  if (intel) {
+    console.info("Intel inside");
+  }
+  if (!menu.length) {
+    return <TextLine>No firmware detected</TextLine>;
+  }
+  return <Tabs menu={menu} />;
+};
+
+Main.propTypes = {
+  data: PropTypes.exact({
+    amd: PropTypes.object,
+    fmap: PropTypes.object,
+    intel: PropTypes.object,
+    uefi: PropTypes.object,
+  }),
+  fileName: PropTypes.string,
+};
 
 const Analyze = () => {
   const [error, setError] = useState(null);
@@ -29,13 +71,15 @@ const Analyze = () => {
       const res = await Promise.allSettled([
         fmap(indata, size),
         utka(indata, size),
+        amdana(indata, size),
       ]);
       setData({
         fmap: JSON.parse(res[0].value),
-        utk: res[1].status === "fulfilled" ? JSON.parse(res[1].value) : {},
+        uefi: res[1].status === "fulfilled" ? JSON.parse(res[1].value) : null,
+        amd: res[2].status === "fulfilled" ? JSON.parse(res[2].value) : null,
       });
       res.forEach((r) => {
-        if (r.status === "rejected") {
+        if (r.status === "rejected" && r.reason) {
           console.error(r.reason);
           setError((errors || []).concat([r.reason]));
         }
@@ -73,7 +117,7 @@ const Analyze = () => {
           <pre>{JSON.stringify(error, null, 2)}</pre>
         </p>
       )}
-      {data && <UEFIImage data={data.utk} fmap={data.fmap} name={fileName} />}
+      {data && <Main data={data} fileName={fileName} />}
       <style jsx>{`
         .error {
           max-width: 420px;
