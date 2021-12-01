dae44dedb028649ca93c1e29ea5c8fbfdeda5ad3
Daniel Maslowski <info@orangecms.org>
2021-12-01 01:27:46 +0100

    shell.nix: reorder, tidy up and remove duplicates

--- shell.nix
+++ shell.nix
@@ -4,18 +4,10 @@ let
   fhs = pkgs.buildFHSUserEnv {
     name = "fiedka";
     targetPkgs = pkgs: with pkgs; [
-      # Copied from another Electron application. Probably doesn't need everything
-      bash
-      coreutils
-      zlib
-      stdenv.cc.cc
-      xorg.libXext
-      xorg.libX11
-      xorg.libXrender
-      xorg.libXtst
-      xorg.libXi
-      xorg.libXft
-      xorg.libxcb
+      nodejs
+      go_1_17
+      git
+
       # common requirements
       freetype
       fontconfig
@@ -23,36 +15,57 @@ let
       gtk2
       gtk3
 
-      nodejs
-      go_1_17
-      git
-      #electron
+      # Copied from another Electron application. Probably doesn't need everything
+      bash
+      coreutils
+      zlib
+      stdenv.cc.cc
+      xorg.libXft
+
+      # Copied from atom
+      dbus
+      atk
+      at-spi2-atk
+      at-spi2-core
+      cairo
+      pango
+      libgnome-keyring3
+      gdk-pixbuf
+      cups
+      expat
+      nspr nss
+
+      xorg.libXrender
+      xorg.libX11
+      xorg.libXext
+      xorg.libXdamage
+      xorg.libXtst
+      xorg.libXcomposite
+      xorg.libXi
+      xorg.libXfixes
+      xorg.libXrandr
+      xorg.libXcursor
+      xorg.libxkbfile
+      xorg.libXScrnSaver
+      xorg.libxcb
+
+      libcap
+      systemd
+      libnotify
+      libsecret
+      libuuid
+      libdbusmenu
+      libdrm
+      mesa # for libgbm
 
       # Manually added
       xorg.libxshmfence
-      nss
-      nspr
-      atk
-      at-spi2-atk
-      dbus
-      libdrm
       libxkbcommon
       alsa-lib
       shared-mime-info
-
-      # Copied from atom
-      stdenv.cc.cc zlib glib dbus gtk3 atk pango freetype libgnome-keyring3
-      fontconfig gdk-pixbuf cairo cups expat nspr nss
-      xorg.libXrender xorg.libX11 xorg.libXext xorg.libXdamage xorg.libXtst
-      xorg.libXcomposite xorg.libXi xorg.libXfixes xorg.libXrandr
-      xorg.libXcursor xorg.libxkbfile xorg.libXScrnSaver libcap systemd libnotify
-      xorg.libxcb libsecret libuuid at-spi2-atk at-spi2-core libdbusmenu
-      libdrm
-      mesa # for libgbm
-      # Not found
     ];
     multiPkgs = null;
-    # Set up path for GTK, otherwise is crashes when using buttons
+    # Set up path for GTK, otherwise is crashes when using file chooser
     profile = with pkgs; ''
       export XDG_DATA_DIRS=/usr/share/:${gsettings-desktop-schemas}/share/gsettings-schemas/${gsettings-desktop-schemas.name}:${gtk3}/share/gsettings-schemas/${gtk3.name}:$XDG_DATA_DIRS
       # TODO: Make sure this works with newer/older versions
