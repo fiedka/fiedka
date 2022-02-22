8659d6f8a741e62fa68c70ffc772376309832c03
Daniel Maslowski <info@orangecms.org>
2022-02-22 01:52:54 +0100

    UEFIImage: add stories

---
+++ src/UEFIImage.stories.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { action } from "@storybook/addon-actions";
+import UEFIImage from "./UEFIImage";
+import fmapData from "./components/flashUsage.json";
+import uefiData from "./UEFI/uefi.json";
+import { UtkContext } from "./context/UtkContext";
+
+export default {
+  title: "UEFI/Image",
+  component: UEFIImage,
+};
+
+export const image = () => (
+  <UtkContext.Provider value={{ remove: action("remove") }}>
+    <UEFIImage data={uefiData} fmap={fmapData} name="OVMF" />
+  </UtkContext.Provider>
+);
