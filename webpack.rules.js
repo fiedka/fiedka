576a4a63354b1974ddbda36329200945db6fea7f
Daniel Maslowski <info@orangecms.org>
2022-03-01 02:30:49 +0100

    Webpack rules: use file-loader for SVG

--- webpack.rules.js
+++ webpack.rules.js
@@ -2,37 +2,37 @@ module.exports = [
   // Add support for native node modules
   {
     test: /\.node$/,
-    use: 'node-loader',
+    use: "node-loader",
   },
   {
     test: /\.(m?js|node)$/,
     parser: { amd: false },
     use: {
-      loader: '@vercel/webpack-asset-relocator-loader',
+      loader: "@vercel/webpack-asset-relocator-loader",
       options: {
-        outputAssetBase: 'native_modules',
+        outputAssetBase: "native_modules",
       },
     },
   },
   {
     test: /\.jsx?$/,
     use: {
-      loader: 'babel-loader',
+      loader: "babel-loader",
       options: {
         exclude: /node_modules/,
-        presets: ['@babel/preset-react']
-      }
-    }
+        presets: ["@babel/preset-react"],
+      },
+    },
   },
   {
     test: /\.tsx?$/,
     exclude: /(node_modules|\.webpack)/,
     use: {
-      loader: 'ts-loader',
+      loader: "ts-loader",
       options: {
-        transpileOnly: true
-      }
-    }
+        transpileOnly: true,
+      },
+    },
   },
   {
     test: /\.go/,
@@ -41,5 +41,11 @@ module.exports = [
         loader: "@fiedka/golang-wasm-async-loader",
       },
     ],
+  },
+  {
+    test: /\.svg?$/,
+    use: {
+      loader: "file-loader",
+    },
   },
 ];
