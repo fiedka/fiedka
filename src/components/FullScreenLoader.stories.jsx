0eedbdbc957b75570973a5d74520b3212499c044
Daniel Maslowski <info@orangecms.org>
2022-03-02 03:14:28 +0100

    FullScreenLoader: add stories

---
+++ src/components/FullScreenLoader.stories.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import FullScreenLoader from "./FullScreenLoader";
+
+export default {
+  title: "FullScreenLoader",
+  component: FullScreenLoader,
+};
+
+export const loader = () => <FullScreenLoader />;
