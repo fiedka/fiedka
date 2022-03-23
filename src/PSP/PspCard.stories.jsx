16ff398ce6d3f9d5c2cab06ab5a172491972b243
Daniel Maslowski <info@orangecms.org>
2022-03-24 00:29:28 +0100

    PSP/PspCard: apply fixd width to story layout

--- src/PSP/PspCard.stories.jsx
+++ src/PSP/PspCard.stories.jsx
@@ -3,12 +3,18 @@ import mftEntry from "../util/mft-entry.fixture.json";
 import mftEntryNoHeader from "../util/mft-entry-no-header.fixture.json";
 import mftEntryNoName from "../util/mft-entry-no-name.fixture.json";
 import { transformEntry } from "../util/mft";
-import PspCard from "./PspCard";
+import PCard from "./PspCard";
 
 export default {
   title: "PSP/PspCard",
-  component: PspCard,
+  component: PCard,
 };
+
+const PspCard = (p) => (
+  <div style={{ width: 480 }}>
+    <PCard {...p} />
+  </div>
+);
 
 export const signedAndPacked = () => <PspCard psp={transformEntry(mftEntry)} />;
 
