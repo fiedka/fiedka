777b2fd2ff39fe783dfedb30049cf3468a8448b4
Daniel Maslowski <info@orangecms.org>
2022-04-24 19:12:54 +0200

    WIP: TPMLog

--- src/TPM/Log.jsx
+++ src/TPM/Log.jsx
@@ -1,4 +1,6 @@
 import React from "react";
+import PropTypes from "prop-types";
+import { Divider } from "@coalmines/indui";
 
 const eventTypes = {
   EV_S_CRTM_VERSION: "CRTM version",
@@ -25,7 +27,7 @@ const transformDigests = (digests = []) =>
 export const transform = (events = []) =>
   events.map((l) => ({
     id: l.EventNum,
-    PCRIndex: l.PCRIndex,
+    pcr: l.PCRIndex,
     type: l.EventType,
     event: l.Event,
     digests: transformDigests(l.Digests),
@@ -42,7 +44,7 @@ const TpmEvent = ({ event }) => {
         .event {
           display: inline;
           max-width: 75%;
-          margin-left: 40px;
+          margin-left: 4px;
           overflow-wrap: break-word;
         }
       `}</style>
@@ -72,14 +74,13 @@ const Entry = ({ data }) => (
     <h2>
       <Type type={data.type} />
     </h2>
-    <div className="flex">
-      <h3>Event</h3>
-      <TpmEvent event={data.event} />
-    </div>
+    <TpmEvent event={data.event} />
+    {/*
     <div className="flex">
       <h3>Digests</h3>
       <Digests digests={data.digests} />
     </div>
+    */}
     <style jsx>{`
       ul h2 {
         background-color: blue;
@@ -99,36 +100,87 @@ const Entry = ({ data }) => (
   </div>
 );
 
+const groupEntries = (events) => {
+  const groups = events.reduce((a, c) => {
+    if ([2, 3, 6].includes(c.pcr)) return a;
+    a[c.pcr] = a[c.pcr] ? [...a[c.pcr], c] : [c];
+    return a;
+  }, {});
+  // TODO: filter empty groups (with only a separator)
+  return groups;
+};
+
+const Log = ({ pcr, group }) => (
+  <section>
+    <header>PCR #{pcr}</header>
+    <ul className="log">
+      {group.map((e) => (
+        <li key={e.id}>
+          {e.type === "EV_SEPARATOR" ? <Divider /> : <Entry data={e} />}
+        </li>
+      ))}
+    </ul>
+    <style jsx>{`
+      ul {
+        padding: 0;
+        list-style: none;
+        font-size: 10px;
+        font-family: sans-serif;
+        word-wrap: wrap;
+      }
+      ul.log {
+        overflow-y: auto;
+        min-width: 350px;
+        max-height: 600px;
+      }
+      li {
+        border: 1px solid blue;
+        padding: 8px;
+      }
+      section {
+        margin: 8px;
+      }
+      header {
+        font-weight: bold;
+      }
+    `}</style>
+  </section>
+);
+
+Log.propTypes = {
+  group: PropTypes.array,
+  pcr: PropTypes.number,
+};
+
 const TPMLog = ({ events = [] }) => {
   return (
-    <>
+    <span>
       <h4>#entries: {events.length}</h4>
-      <ul className="log">
-        {events.map((e) => (
-          <li key={e.id}>
-            <Entry data={e} />
-          </li>
-        ))}
-        <style jsx>{`
-          ul {
-            list-style: none;
-            font-size: 10px;
-            font-family: sans-serif;
-            word-wrap: wrap;
-          }
-          ul.log {
-            overflow-y: scroll;
-            width: 580px;
-            max-height: 600px;
-          }
-          li {
-            border: 1px solid blue;
-            padding: 8px;
-          }
-        `}</style>
-      </ul>
-    </>
+      <div className="flex">
+        {Object.entries(groupEntries(events))
+          .filter(([pcr]) => pcr < 8)
+          .map(([pcr, group]) => (
+            <Log pcr={pcr} group={group} key={pcr} />
+          ))}
+      </div>
+      <div className="flex">
+        {Object.entries(groupEntries(events))
+          .filter(([pcr]) => pcr >= 8)
+          .map(([pcr, group]) => (
+            <Log pcr={pcr} group={group} key={pcr} />
+          ))}
+      </div>
+      <style jsx>{`
+        .flex {
+          display: flex;
+        }
+      `}</style>
+    </span>
   );
+};
+
+TPMLog.propTypes = {
+  events: PropTypes.array,
 };
 
 export default TPMLog;
