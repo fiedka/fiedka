17b9dd127daeeae6373c324de9a5060f3b947cb0
Daniel Maslowski <info@orangecms.org>
2022-07-23 15:26:59 +0200

    rework download helper to offer JSON download

--- src/util/download.js
+++ src/util/download.js
@@ -1,4 +1,5 @@
 const dblobPre = "data:application/octet-stream;charset=utf-16le;base64,";
+const jblobPre = "data:text/json;charset=utf-8,";
 
 const b64enc = (u8a) => {
   const out = [];
@@ -8,12 +9,22 @@ const b64enc = (u8a) => {
   return btoa(out.join(""));
 };
 
-export const download = (fileName, content) => {
+const download = (fileName, data) => {
   const e = document.createElement("a");
-  e.setAttribute("href", dblobPre + b64enc(content));
+  e.setAttribute("href", data);
   e.setAttribute("download", fileName);
   e.style.display = "none";
   document.body.appendChild(e);
   e.click();
   document.body.removeChild(e);
+};
+
+export const downloadU8a = (fileName, content) => {
+  const data = b64enc(content);
+  download(fileName, dblobPre + data);
+};
+
+export const downloadJson = (fileName, content) => {
+  const data = encodeURIComponent(JSON.stringify(content, null, 2));
+  download(fileName, jblobPre + data);
 };
